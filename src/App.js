ac630837783908f99ef5c1b55e669dec502e77c8
vignesh57001 <vigneshmenon2303@gmail.com>
2022-10-13 11:40:26 +0530

    new

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div></div>
+    <section  className="pricing py-5">
+      <div  className="container">
+        <div  className="row">
+        <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">Free</h5>
+            </div> </div> </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
